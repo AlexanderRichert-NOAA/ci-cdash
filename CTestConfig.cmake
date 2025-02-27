976a6df5d2274136f6a5ca35281277f4c2cbc2fd
Alex Richert <alexander.richert@noaa.gov>
2025-02-26 17:50:55 -0800

    Update CTestConfig.cmake

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,7 +10,7 @@ set(CTEST_NIGHTLY_START_TIME 01:00:00 UTC)
 
 set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=NCEPLIBS+test")
+set(CTEST_DROP_LOCATION "/submit.php?project=${CTEST_PROJECT_NAME}")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(CTEST_BINARY_DIRECTORY build)
