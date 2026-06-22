4931e6680bc4835125b52d39b23339cea572e447
Alex Richert <alexander.richert@noaa.gov>
2026-06-22 12:14:38 -0700

    try ci-profile-tests as submodule

--- RunCDash.cmake
+++ RunCDash.cmake
@@ -4,14 +4,7 @@ set(CTEST_DROP_LOCATION "/submit.php?project=${CTEST_PROJECT_NAME}")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 if(ENABLE_PROFILING)
-    include(FetchContent)
-    FetchContent_Declare(
-        ci_profile_tests
-        GIT_REPOSITORY https://github.com/AlexanderRichert-NOAA/ci-profile-tests.git
-        GIT_TAG        main          # pin to a tag/SHA for reproducibility
-    )
-    FetchContent_MakeAvailable(ci_profile_tests)
-    include("${ci_profile_tests_SOURCE_DIR}/cmake/Profiling.cmake")
+  include("${CTEST_SOURCE_DIRECTORY}/ci-profile-tests/cmake/Profiling.cmake")
 endif()
 
 if(GITHUB_ACTIONS)
